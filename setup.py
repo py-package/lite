8ec124754db560186f195670626b18bfc48e49b4
yubarajshrestha <companion.krish@gmail.com>
2023-03-23 11:13:09 +0545

    Added Setup Py and github workflow

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lite",
+    name="lite-gs",
     version="0.1.0",
-    description="A simple web framework built from scratch",
+    description="Lite-gs is a lightweight Python web framework designed for simplicity and ease of use.",
     author="Yubaraj Shrestha",
     author_email="yubaraj@py-package.com",
     url="https://github.com/py-package/lite",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
     install_requires=[
         "waitress",
         "click",
@@ -37,9 +38,13 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     python_requires=">=3.6",
+    keywords="Python, Framework, Lite, Web, Web Framework, Python Framework",
     entry_points={
         "console_scripts": [
             "lite=lite.cli:lite",
         ],
+    },
+    package_data={
+        '': [],
     },
 )
